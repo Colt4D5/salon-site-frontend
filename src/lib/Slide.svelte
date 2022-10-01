a9fa95ee97c62f5dd9c5f508e98d358eeee8655c
Colton <allen.colton@hotmail.com>
2022-10-01 16:57:56 -0700

    connected graphql and swiper.js

---
+++ src/lib/Slide.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let slide;
+  
+  import { SwiperSlide } from 'swiper/svelte';
+</script>
+
+<SwiperSlide>
+  <picture>
+    <source media="(max-width: 799px)" srcset="http://localhost:1337{slide.attributes.img_mobile.data.attributes.url}">
+    <source media="(min-width: 800px)" srcset="http://localhost:1337{slide.attributes.img_full.data.attributes.url}">
+    <img src="http://localhost:1337{slide.attributes.img_full.data.attributes.url}" alt="{ slide.attributes.title }" />
+  </picture>
+</SwiperSlide>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<!-- Swiper's CSS -->
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/swiper/swiper-bundle.min.css"
+	/>
+</svelte:head>
+
+<script>
+  import Slide from "../lib/Slide.svelte";
+
+  // Import Swiper Svelte components
+  import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+  import { Swiper } from 'swiper/svelte';
+
+  // Import Swiper styles
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  export let data;
+  const slides = data.sliders.data;
+</script>
+
+<h1>Home Page</h1>
+<Swiper
+    id="heroimage"
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    spaceBetween={50}
+    navigation
+    pagination={{ clickable: true }}
+  >
+  { #each slides as slide }
+    <Slide slide={slide} />
+  {/each}
+</Swiper>
