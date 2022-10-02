e242c3042b6480f9f60f3f7dbfd7f2cfcb712bff
Colton <allen.colton@hotmail.com>
2022-10-01 19:35:02 -0700

    added navigation object

---
+++ src/lib/navigation.js
@@ -0,0 +1,60 @@
+export const nav = [
+  {
+    title: 'Services',
+    link: '/services/hair/',
+    children: [
+      {
+        title: 'Hair',
+        link: '/services/hair/',
+      },
+      {
+        title: 'Color',
+        link: '/services/color/',
+      }
+    ]
+  },
+  {
+    title: 'About',
+    link: '/about/',
+    children: [
+      {
+        title: 'About Us',
+        link: '/about/',
+      },
+      {
+        title: 'Contact Us',
+        link: '/about/contact/',
+      },
+      {
+        title: 'Reviews',
+        link: '/about/reviews/',
+      },
+      {
+        title: 'Blog',
+        link: '/blog/',
+      }
+    ]
+  },
+  {
+    title: 'Aveda',
+    link: '/aveda/why-aveda/',
+    children: [
+      {
+        title: 'Why Aveda',
+        link: '/aveda/why-aveda/',
+      },
+      {
+        title: 'Aveda Plus Rewards',
+        link: '/aveda/aveda-plus-rewards/',
+      },
+      {
+        title: 'Aveda Styling Videos',
+        link: '/aveda/aveda-styling-videos/',
+      },
+      {
+        title: 'New at Aveda',
+        link: '/aveda/new-at-aveda/',
+      }
+    ]
+  }
+]
