1664e4d57a2d463e7a735c88f200d6d3c35f491f
Colton <allen.colton@hotmail.com>
2022-10-01 19:27:45 -0700

    added +layout with header & footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer>
+  <!-- <div id="footer-top">
+    <div id="inner" class="row">
+      <div class="wrap-left large-7 column">
+        <div id="foot1" class="medium-6 large-5 column">
+          {{ footer_column_1 }}
+        </div>
+        <div id="foot2" class="medium-6 large-7 column">
+          {{ footer_column_2 }}
+          <div class="social-icons-wrap show-for-medium">
+            {% include 'components/social_icons.twig' %}
+          </div>
+        </div>
+      </div>
+      <div class="wrap-right large-5 column">
+        <div id="foot3" class="medium-6 large-5 column">
+          {{ footer_column_3 }}
+          <div class="social-icons-wrap hide-for-medium">
+            {% include 'components/social_icons.twig' %}
+          </div>
+        </div>
+        <div id="foot4" class="medium-6 large-7 column">
+          {{ footer_column_4 }}
+        </div>
+      </div>
+    </div>
+  </div> -->
+  <div id="footer-bottom">
+    <p class="source-org copyright">&copy; 2022 Site Name. All rights reserved. <a href="/privacy-policy/">Privacy/Terms</a>.  Made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://imaginalmarketing.com" target="_blank">Imaginal Marketing Group</a></p>
+  </div> <!-- end #footer-bottom -->
+</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<!-- Swiper's CSS -->
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/swiper/swiper-bundle.min.css"
+	/>
+</svelte:head>
+
+<script>
+  import Footer from "../lib/components/Footer.svelte";
+  import Header from "../lib/components/Header.svelte";
+
+  import '../style/index.scss';
+</script>
+
+<Header />
+<slot></slot>
+<Footer />
